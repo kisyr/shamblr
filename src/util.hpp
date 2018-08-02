d39b5ce50bd43365bfa596274a005511324d1704
Kim Syrjälä <kim.syrjala@gmail.com>
2018-08-02 11:32:10 +0000

    Added util for triangle vertices

--- src/util.hpp
+++ src/util.hpp
@@ -5,6 +5,21 @@
 namespace shamblr {
 namespace util {
 
+size_t generateTriVertices(
+	const glm::vec3& min,
+	const glm::vec3& max,
+	std::vector<glm::vec3>& out
+) {
+	const glm::vec3 vertices[] = {
+		{ min.x, min.y, min.z },
+		{ (max.x - min.x) / 2, min.y, max.z },
+		{ max.x, min.y, min.z },
+	};
+	const auto count = sizeof(vertices) / sizeof(vertices[0]);
+	out.insert(out.end(), vertices, vertices + count);
+	return count;
+}
+
 size_t generateRectVertices(
 	const glm::vec3& min,
 	const glm::vec3& max,
